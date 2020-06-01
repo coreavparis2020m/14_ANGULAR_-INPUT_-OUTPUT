b600ab0d427bd074ff329cfcf492dd3dc84e56e1
“coreavparis2020m” <coreavparis2020m@gmail.com>
2020-06-01 13:06:09 +0200

    initial

---
+++ src/app/jugadores/jugadores.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-jugadores',
+  templateUrl: './jugadores.component.html',
+  styleUrls: ['./jugadores.component.scss']
+})
+export class JugadoresComponent implements OnInit {
+
+    @Input() jugadores; // Esta propiedad recibirá valores desde su componente padre
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
